1472662a0c18f2097ca31a6cc8c24812cd15e4be
Anand987 <anandsharma110397@gmail.com>
2024-05-11 19:05:15 +0530

    added footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/custom/Header";
+import { Footer } from "@/components/custom/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default async function RootLayout({
       <body className={inter.className}>
         <Header data={globalData.header} />
         {children}
+        <Footer data={globalData.footer} />
       </body>
     </html>
   );
